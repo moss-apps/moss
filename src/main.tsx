fa585c823097131695f86782ee3abe79a9e3fc26
ultraelectronica <fyketonel@gmail.com>
2026-06-14 23:57:46 +0800

    Add React Router with layout and initial routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import App from "./App"
+import { BrowserRouter, Routes, Route } from "react-router"
+import { Layout } from "@/components/Layout"
+import { Home } from "@/pages/Home"
+import { Changelog } from "@/pages/Changelog"
 import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/changelog" element={<Changelog />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
